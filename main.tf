06b32aa635c8fc69a4cb78f75bd9102ece80cfda
David Crammer <dcrammer777@gmail.com>
2023-10-09 10:11:46 -0700

    Provision a droplet and add github actions workflow

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "example" {
+  image  = "ubuntu-20-04-x64"
+  name   = "example-droplet"
+  region = "sfo3"
+  size   = "s-1vcpu-1gb"
+
+  tags = [
+    "web",
+    "example",
+  ]
+}
