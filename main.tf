c70862b9777ecfdf74c87245e0d60fa9409695ed
David Crammer <dcrammer777@gmail.com>
2023-10-19 10:06:57 -0700

    Change auth for k8s provider

--- main.tf
+++ main.tf
@@ -11,6 +11,10 @@ terraform {
       source  = "integrations/github"
       version = ">=5.18.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "1.7"
+    }
   }
 }
 
@@ -31,8 +35,7 @@ resource "digitalocean_kubernetes_cluster" "cluster" {
 }
 
 provider "kubernetes" {
-  host                   = digitalocean_kubernetes_cluster.cluster.endpoint
-  client_certificate     = base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.client_certificate)
-  client_key             = base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.client_key)
+  host  = digitalocean_kubernetes_cluster.cluster.endpoint
+  token = digitalocean_kubernetes_cluster.cluster.kube_config.0.token
   cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
 }
